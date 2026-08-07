2e0b19760b95c242cd619b19916e05a5557d372c
Sigmanificient <edhyjox@gmail.com>
2026-08-07 16:56:11 +0200

    Extract lib utilities

--- src/default.nix
+++ src/default.nix
@@ -1,26 +1,32 @@
 #!/usr/bin/env -S nix eval --raw -f
+
 with builtins; let
+  inherit (import ./lib.nix) prepend join range toInt toFloat;
+
   image = {
     width = 256;
     height = 256;
+
+    maxColorValue = 255;
   };
 
-  range = count: genList (x: x) count;
-
   makePixel = i: j: let
-    r = (1.0 * i) / (image.width - 1);
-    g = (1.0 * j) / (image.height - 1);
+    r = (toFloat i) / (image.width - 1);
+    g = (toFloat j) / (image.height - 1);
     b = 0.0;
 
-    flatten = c: toString (floor (c * 255.999));
-  in "${flatten r} ${flatten g} ${flatten b}";
+    m = image.maxColorValue + 0.999;
+    flatten = c: toInt (c * m);
+  in map flatten [r g b];
 
-  pixels = (
-    concatLists (
-      map (j: map (i: makePixel i j) (range image.width)) (range image.height)
-    )
-  );
+  pixels = concatMap
+    (j: map (i: makePixel i j) (range image.width))
+    (range image.height);
 
 in
-  "P3\n${toString image.width} ${toString image.height}\n255\n"
-  + (concatStringsSep "\n" pixels) + "\n"
+ (concatStringsSep "\n"
+    (prepend
+      (join " " ["P3" image.width image.height image.maxColorValue])
+      (map (join " ") pixels)
+    )
+  ) + "\n"
---
+++ src/lib.nix
@@ -0,0 +1,22 @@
+with builtins; let
+  id = x: x;
+
+  join = sep: lst: concatStringsSep sep (map toString lst);
+
+  prepend = item: lst: [item] ++ lst;
+
+  range = count: genList id count;
+
+  toFloat = x: 1.0 * x;
+
+  toInt = floor;
+in {
+  inherit
+    id
+    join
+    prepend
+    range
+    toInt
+    toFloat
+  ;
+}
