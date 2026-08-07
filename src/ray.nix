545db823a459f0d964b85349c12d6e172eb6b7d8
Sigmanificient <edhyjox@gmail.com>
2026-08-07 23:41:12 +0200

    Colorize the sphere

--- src/ray.nix
+++ src/ray.nix
@@ -1,9 +1,11 @@
 let
+  vec3 = import ./vec3.nix;
+
   ray = origin: direction: {
     inherit origin direction;
   };
 in {
   __functor = self: ray;
 
-  at = r: t: r.origin + t * r.drection;
+  at = r: t: vec3.add r.origin (vec3.scale t r.direction);
 }
